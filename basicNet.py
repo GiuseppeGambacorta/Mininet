77ae903aa889d4d7620cf38636b22555e977797b
None <None>
2024-10-28 06:15:23 -0700

    first file

---
+++ basicNet.py
@@ -0,0 +1,39 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+
+class CustomTopo(Topo):
+    def build(self, n=3):
+        switch = self.addSwitch('s1')
+    
+        for i in range(n):
+            host = self.addHost(f'h{i+1}')
+            self.addLink(host, switch)
+
+
+def createNetwork():
+    topo = CustomTopo(n=3)
+    net = Mininet(topo=topo)
+    
+    info('*** Starting network\n')
+    net.start()
+
+    info('*** Assigning IP addresses\n')
+    net.get('h1').setIP('10.0.0.1/24')
+    net.get('h2').setIP('10.0.0.2/24')
+    net.get('h3').setIP('10.0.0.3/24')
+    
+    info('*** Testing network connectivity\n')
+    net.pingAll()
+    
+    info('*** Running CLI\n')
+    CLI(net)
+    
+    info('*** Stopping network\n')
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    createNetwork()
